0784c2e4b587631a6cef2af65f660e3a86745230
Nathan <rocky@askoutthere.com>
2019-05-07 13:32:05 -0500

    formTriangle

--- ChallengesWithTestsMarkVII/ChallengesSet04.cs
+++ ChallengesWithTestsMarkVII/ChallengesSet04.cs
@@ -40,7 +40,14 @@ namespace ChallengesWithTestsMarkVII
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            if (sideLength1 + sideLength2 > sideLength3 && sideLength1 + sideLength3 > sideLength2 && sideLength2 + sideLength3 > sideLength1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool IsStringANumber(string input)
